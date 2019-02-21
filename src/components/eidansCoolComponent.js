ed480b9bf9420cca8cfe9443d920826ee12e9469
victorlaucas <victorlaucas@gmail.com>
2019-02-21 15:20:34 -0700

    Made changes

--- src/components/eidansCoolComponent.js
+++ src/components/eidansCoolComponent.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function EidansCoolComponent(){
     return(
         <div className='EidansCoolComponent'>
-            The coolest component ever
+            The lamest component ever
+
+            <h3>I'm 15 and this is deep</h3>
         </div>
     )
 }
