18c100a903cde82be54a9ed2de8520d509b55648
victorlaucas <victorlaucas@gmail.com>
2019-02-21 15:16:22 -0700

    Created new component

---
+++ src/components/myClass.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyClass extends Component {
+  render() {
+    return (
+      <div>
+        COOL BEANS
+      </div>
+    );
+  }
+}
+
+export default MyClass;
